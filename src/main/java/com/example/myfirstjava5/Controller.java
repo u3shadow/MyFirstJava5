b13a19f339eaa8b9b0a7a5dd4aa521771b01b001
xiaolei <xiaolei@seeyon.com>
2023-06-07 22:02:13 +0800

    添加接口hello

--- src/main/java/com/example/myfirstjava5/Controller.java
+++ src/main/java/com/example/myfirstjava5/Controller.java
@@ -1,2 +1,14 @@
-package com.example.myfirstjava5;public class Controller {
+package com.example.myfirstjava5;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "调用Hello接口";
+    }
 }
